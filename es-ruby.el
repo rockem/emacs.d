9c474b0ab7f960ed7ab18a15665cf5023b87347c
elis <eli.segal@gmail.com>
2011-07-04 13:07:00 +0300

    Added rhtml mode

--- es-ruby.el
+++ es-ruby.el
@@ -1,15 +1,23 @@
 ;;(add-to-list 'load-path (expand-file-name (concat site-lisp-path "ruby")))
 ;;(add-to-list 'load-path (expand-file-name (concat site-lisp-path "rails-reloaded")))
-(add-to-list 'load-path (expand-file-name (concat site-lisp-path "rspec-mode")))
 (add-to-list 'load-path (expand-file-name (concat site-lisp-path "rinari")))
+(add-to-list 'load-path (expand-file-name (concat site-lisp-path "rspec-mode")))
+(add-to-list 'load-path (expand-file-name (concat site-lisp-path "rhtml-mode")))
 
 (require 'ruby-mode)
 
 (require 'rinari)
 (setq rinari-tags-file-name "TAGS")
 
-
 (require 'rspec-mode)
+
+(add-to-list 'load-path "~/path/to/your/elisp/rhtml")
+(require 'rhtml-mode)
+(add-hook 'rhtml-mode-hook (lambda () (rinari-launch)))
+
+;;(autoload 'rhtml-mode "rhtml-mode" "RHTML" t)
+;;(add-to-list 'auto-mode-alist '("\\.rhtml$" . rhtml-mode))
+;;(add-to-list 'auto-mode-alist '("\\.html\.erb$" . rhtml-mode)) 
 
 ;;(add-to-list 'auto-minor-mode-alist
 ;;	     (cons (concat "\\." (regexp-opt '("html\\.erb" "rb") t) "\\'")
